2ea23d5028b482cbaedb694ef308fd41de368ede
Mark Nuiver <mark@vrijblikken.nl>
2016-08-28 13:00:09 +0200

    Add new song almost working...

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   root "artists#index"
 
-  resources :artists
-  resources :songs
-
+  resources :artists do
+    resources :songs
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
